85020bc882c82f6e437a4b94269f239f1cad8fc4
Sarath kumar <sarathkumars677@gmail.com>
2015-09-11 11:53:01 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,25 +1,5 @@
  
  var myApp = angular.module('myApp',[]);
-// myApp.factory('Data',function(){
-// 	return {message:""}
-// }) 
-
-
-// myApp.filter('reverse',function()
-// {
-// 		return function(text)
-// 		{
-// 	  	return  text.split("").reverse().join("");
-
-// 		} 
-// }) 
-
-
-// myApp.controller('myctrl',function($scope,Data) 
-// { 
-//  	 $scope.data=Data;
- 	  
-//  });
  myApp.controller('Counter',function ($scope,$timeout) {
     $scope.counter = 50;
     $scope.onTimeout = function(){
